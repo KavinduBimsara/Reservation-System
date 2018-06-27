20c4654927450b99258a8c3bb469f5c76a43130b
Muhyadin R. Abdullahi <muhi3849@gmail.com>
2018-06-27 22:45:21 +0300

    Edit Route.php file
    Removed redundant route declaration.

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('users/{id}/delete', 'Backend\UsersController@destroy')->name('users.delete');
 
+Route::get('users/{id}/delete', 'Backend\UsersController@destroy')->name('users.delete');
 Route::get('amenities/{id}/delete', 'Backend\AmenitiesController@destroy')->name('amenities.delete');
-Route::get('amenities/{id}/edit', 'Backend\AmenitiesController@edit')->name('amenities.edit');
 
 Route::resources([
     'users'=> 'Backend\UsersController',
