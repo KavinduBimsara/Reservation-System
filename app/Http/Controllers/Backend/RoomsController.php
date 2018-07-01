69dc99c66455613492605eedd6dab74b7c09d6b1
Muhyadin Abdullahi <muhi3849@gmail.com>
2018-07-01 20:26:26 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Backend/RoomsController.php
+++ app/Http/Controllers/Backend/RoomsController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Models\Room;
-use Illuminate\Http\Request;
-
 use App\Models\Amenity;
-
-use App\Http\Controllers\Controller;
 use App\Models\RoomType;
+use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use App\Http\Controllers\Controller;
 use Yajra\DataTables\Facades\DataTables;
 use App\Http\Requests\Store\StoreRoomRequest;
 use App\Http\Requests\Update\UpdateRoomRequest;
@@ -80,14 +78,13 @@ class RoomsController extends Controller
      */
     public function store(StoreRoomRequest $request)
     {
-
         $room = Room::create([
             'room_no'      => $request->room_no,
             'name'         => $request->name,
             'description'  => $request->description,
             'room_type_id' => $request->room_type_id,
         ]);
-        
+
         $room->amenity()->attach($request->amenities);
 
         return redirect()->route('rooms.index');
